210a5af2db460ff4d416844f65be9497dc41dc5e
Pekka Lukkari <pekkaj.lukkari@gmail.com>
2016-02-25 09:22:51 +0200

    Cleaned a few unnecessary files

--- TrelloDashboard/App_Start/BundleConfig.cs
+++ TrelloDashboard/App_Start/BundleConfig.cs
@@ -11,8 +11,7 @@ namespace TrelloDashboard
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
